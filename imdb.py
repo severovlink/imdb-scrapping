9a1b65cef0c871fa107987c773d9ccdafb69563d
severovlink <severovlink@gmail.com>
2019-07-12 12:01:23 +0300

    First Commit

---
+++ imdb.py
@@ -0,0 +1,32 @@
+"""Script to gather content from IMDB"""
+import sys
+
+
+def main():
+    """
+    Main entry point for the script.
+    :return:
+    """
+    pass
+
+
+def get_top_grossing_movie_link(url):
+    """
+    Return a list of tuple containing the top grossing movies of 2019 and link to their IMDB page.
+    :param url:
+    :return top_movie_list:
+    """
+    pass
+
+
+def get_keywords_for_movie(url):
+    """
+    return of keywords associated with 'movie'.
+    :param url:
+    :return keywords_list:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
