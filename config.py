1f77529637c3235f0782704e3e6f364e9435adc4
severovlink <severovlink@gmail.com>
2019-07-16 20:38:55 +0300

    Add body to general functions

---
+++ config.py
@@ -0,0 +1,1 @@
+URL = 'https://www.imdb.com/search/title/?year=2019-01-01,2019-12-31'
--- imdb.py
+++ imdb.py
@@ -1,22 +1,39 @@
 """Script to gather content from IMDB"""
 import sys
+import csv
+from bs4 import BeautifulSoup
+import requests
+import config
 
 
-def main():
+URL = config.URL
+
+
+def first_test():
     """
     Main entry point for the script.
     :return:
     """
-    pass
+    movies = get_top_movie_link(URL)
+    with open('output.csv', 'w') as output:
+        writer = csv.writer(output)
+        for title, url in movies:
+            keywords = get_keywords_for_movie('http://www.imdb.com{}keywords/'.format(url))
+            writer.writerow([title, keywords])
 
 
-def get_top_grossing_movie_link(url):
+def get_top_movie_link(url):
     """
     Return a list of tuple containing the top grossing movies of 2019 and link to their IMDB page.
     :param url:
     :return top_movie_list:
     """
-    pass
+    response = requests.get(url)
+    movies_list = []
+    for each_url in BeautifulSoup(response.text, 'html.parser').select('a[href*="title"]'):
+        movie_title = each_url.text
+        movies_list.append((movie_title, each_url['href']))
+    return movies_list
 
 
 def get_keywords_for_movie(url):
@@ -25,8 +42,12 @@ def get_keywords_for_movie(url):
     :param url:
     :return keywords_list:
     """
-    pass
+    keywords = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all('table', class_='dataTable')
+    table = tables[0]
+    return [td.text for tr in table.find_all('tr') for td in tr.find_all('td')]
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+print(get_top_movie_link(URL))
